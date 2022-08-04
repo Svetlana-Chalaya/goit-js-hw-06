78b37e77ce5a1a8779daafc672bcba897c4946f7
Svetlana-Chalaya <as-torg@ua.fm>
2022-08-04 12:45:00 +0300

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,38 @@
+"use strict";
+
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const makeElementsLi = document.createElement(`li`);
+makeElementsLi.classList.add(`.item`);
+
+makeElementsLi.append(...elements);
+
+document.ul.appendChild(makeElementsLi);
+console.log(document.ul);
+console.log(makeElementsLi);
+
+// const makeElementsLi = ingredients;
+
+// const elements = ingredients.map((option) => {
+//   ingredients.classList.add(`.item`);
+//   makeElementsLi.append(...elements);
+
+//   return elements;
+// });
+// console.log(makeElementsLi);
+
+// makeElementsLi.classList.add(ingredients);
+// makeElementsLi.textContent = `
+// "Potatoes",
+//   "Mushrooms",
+//   "Garlic",
+//   "Tomatos",
+//   "Herbs",
+//   "Condiments",`;
