4ef1fb58c51aae08f37c07ca68dd520481808873
Svetlana-Chalaya <as-torg@ua.fm>
2022-08-07 16:42:09 +0300

    task-05

--- js/task-05.js
+++ js/task-05.js
@@ -1,8 +1,27 @@
 "use strict";
 
-const textInput = document.querySelector("#name-input");
-const output = document.querySelector("#name-output");
+const inputText = document.querySelector("#name-input");
+const outputText = document.querySelector("#name-output");
 
-textInput.addEventListener("input", (event) => {
-  output.textContent = event.currentTarget.#name - output;
-});
+const input = document.getElementById("name-input");
+const output = document.getElementById("name-output");
+
+input.oninput = function () {
+  if (input.value === "") {
+    output.innerHTML = "Anonymous";
+  } else {
+    output.innerHTML = input.value;
+  }
+};
+
+// const refs = {
+//   input: document.querySelector("#name-input"),
+//   output: document.querySelector("#name-output > span"),
+// };
+
+// refs.input.addEventListener("input", onInputChange);
+
+// function onInputChange(event) {
+//   // console.log(event.currentTarget.value);
+//   refs.output.textContent = event.currentTarget.value;
+// }
