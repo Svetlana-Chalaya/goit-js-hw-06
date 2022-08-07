e5f2700954cf332f27a416d3f78afaad0efdbbd3
Svetlana-Chalaya <as-torg@ua.fm>
2022-08-07 20:30:03 +0300

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+"use strict";
 
+const form = document.querySelector("form");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+
+  console.log(`Login: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
