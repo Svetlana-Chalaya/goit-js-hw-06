16d04dd74129687635235f6f0ec03c9e1041fab7
Svetlana-Chalaya <as-torg@ua.fm>
2022-08-07 20:30:13 +0300

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+"use strict";
+
+const body = document.querySelector("body");
+
+const setBg = () => {
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  document.body.style.backgroundColor = "#" + randomColor;
+  color.innerHTML = "#" + randomColor;
+};
+
+body.addEventListener("click", setBg);
+setBg();
+
+console.log(setBg);
+console.log(body);
