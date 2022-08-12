53271d6ce2ccb125db3b1b634f1ce91d697942c7
Svetlana-Chalaya <as-torg@ua.fm>
2022-08-12 09:52:41 +0300

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -14,7 +14,7 @@ const ingredientsUl = document.querySelector(`ul`);
 const foodIngredients = ingredients.map((ingredient) => {
   const makeElementsLi = document.createElement(`li`);
   makeElementsLi.classList.add("item");
-  makeElementsLi.textContent = ingredients;
+  makeElementsLi.textContent = ingredient;
 
   return makeElementsLi;
 });
