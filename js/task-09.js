5c4acdb8aa663000189c47ad648ca5b1c8e7ce69
Svetlana-Chalaya <as-torg@ua.fm>
2022-08-12 09:53:03 +0300

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,15 +1,39 @@
 "use strict";
 
 const body = document.querySelector("body");
+const changeColor = querySelector(".color");
 
-const setBg = () => {
-  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+body.addEventListener("click", getRandomHexColor);
+getRandomHexColor();
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+body.addEventListener("click", getRandomHexColorBody);
+getRandomHexColorBody();
+
+function getRandomHexColorBody() {
   document.body.style.backgroundColor = "#" + randomColor;
   color.innerHTML = "#" + randomColor;
-};
+}
 
-body.addEventListener("click", setBg);
-setBg();
-
-console.log(setBg);
+console.log(changeColor);
 console.log(body);
+
+// function getRandomHexColor() {
+//   return (
+//     `#${Math.floor(Math.random() * 16777215)
+//       .toString(16)
+//       .padStart(6, 0)}`
+//     (document.body.style.backgroundColor = "#" + randomColor),
+//   color.innerHTML = "#" + randomColor),
+// };
+
+// const setBg = () => {
+//   const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+//   document.body.style.backgroundColor = "#" + randomColor;
+//   color.innerHTML = "#" + randomColor;
+// };
